d8e7464855c696ebc250031fc3a4d10bdd4519fc
Karen V <karenvo@usc.edu>
2019-08-21 02:04:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,7 +104,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade <= 11
+      WHERE grade = 10
       LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
